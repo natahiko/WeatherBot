d43ae9e06cf976a1a292ea875bd09ca4e3aa40e4
natahiko <n.shkarow@gmail.com>
2019-07-21 12:32:34 +0300

    copied methods

--- app/classes/schedule_timer.py
+++ app/classes/schedule_timer.py
@@ -3,7 +3,6 @@ import time
 from pyowm import OWM
 import mysql.connector
 import telebot
-from methods import methods
 import requests
 import datetime
 
@@ -38,13 +37,60 @@ def send_day_weather_to_user(userid, cityid):
         'http://api.openweathermap.org/data/2.5/forecast?id={}&type=like&APPID={}'.format(cityid, APPID))
     data = r.json()['list']
     today = datetime.date.today() + datetime.timedelta(days=1)
-    mess = "Weather for today (" + str(today.day) + "." + str(today.month) + "): \n" + str(
-        methods.get_day_weather(1, data))
+    mess = "Weather for today ({0}.{1}): \n{2}".format(str(today.day), str(today.month), str(
+        get_day_weather(1, data)))
     bot.send_message(userid, mess)
 
+
+def get_weather_icon(icon):
+    if icon == '01d' or icon == '01n': return '☀';
+    if icon == '02d' or icon == '02n': return '⛅';
+    if icon == '03d' or icon == '03n': return '☁';
+    if icon == '04d' or icon == '04n': return '💨';
+    if icon == '09d' or icon == '09n': return '🌧';
+    if icon == '10d' or icon == '10n': return '🌦';
+    if icon == '11d' or icon == '11n': return '⛈';
+    if icon == '13d' or icon == '13n': return '❄';
+    if icon == '50d' or icon == '50n': return '🌫';
+
+
+def get_wind_smiley(deg):
+    if deg > 350 or deg < 10: return '➡';
+    if deg > 80 and deg < 100: return '⬆';
+    if deg > 170 and deg < 190: return '⬅';
+    if deg > 260 and deg < 280: return '⬇';
+    if deg > 10 and deg < 80: return '↗';
+    if deg > 100 and deg < 170: return '↖';
+    if deg > 190 and deg < 260: return '↙';
+    if deg > 280 and deg < 350: return '↘';
+
+
+def get_day_weather(day, data):
+    now_hour = int(int(round((24 - datetime.datetime.now().hour) / 3)) + 1)
+    if day == 1:
+        data = data[0:(now_hour + 1)]
+        hour = 24 - now_hour * 3
+    else:
+        data = data[day * 8 - 8 + now_hour:day * 8 + now_hour]
+        hour = 0
+    result = ""
+    for x in data:
+        print([x][0])
+        w = [x][0]
+        if hour < 10: result += '0'
+        result += str(hour) + ":00: {} {} ({})\n     🌡 {}° {} {}m/s \n".format(w['weather'][0]['main'],
+                                                                                get_weather_icon(
+                                                                                    w['weather'][0]['icon']),
+                                                                                w['weather'][0]['description'],
+                                                                                int(round(w['main']['temp'] - 273, 15)),
+                                                                                get_wind_smiley(w['wind']['deg']),
+                                                                                w['wind']['speed'])
+        hour += 3
+    return result
 
 # do method everyday at 7:00
 schedule.every().day.at("07:00").do(msg)
 while True:
     schedule.run_pending()
     time.sleep(10)
+
